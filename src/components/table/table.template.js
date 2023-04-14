8f0af702a628e570eb9cefcdd1ae1e3dc3c415f3
Oleg <lozovoi_oleg@mail.ru>
2023-04-14 10:52:52 +0300

    finish base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,18 @@ const CODES = {
     'Z': 90
 }
 
-function toCell(_, col) {
-    return `
-        <div class="cell" contenteditable="" data-col="${col}"></div>
-    `
+function toCell(row) {
+    return function (_, col) {
+        return `
+            <div 
+            class="cell" 
+            contenteditable="" 
+            data-col="${col}" 
+            data-type="cell"
+            data-id="${row}:${col}"
+            ></div>
+        `
+    }
 }
 
 function toColumn(col, index) {
@@ -47,12 +55,12 @@ export function createTable(rowsCount = 15) {
 
     rows.push(createRow(cols))
 
-    for (let i = 0; i < rowsCount; i++) {
+    for (let row = 0; row < rowsCount; row++) {
         const cells = new Array(colsCount)
             .fill('')
-            .map((el, index) => toCell(el, index))
+            .map(toCell(row))
             .join('')
-        rows.push(createRow(cells, i+1))
+        rows.push(createRow(cells, row+1))
     }
 
     return rows.join('')
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,11 +4,28 @@ export class ExcelComponent extends DomListener{
     constructor($root, options = {}) {
         super($root, options.listeners)
         this.name = options.name || ''
+        this.emitter = options.emitter
+        this.unsubscribes = []
+
+        this.prepare()
+    }
+
+    prepare() {
+
     }
 
     //Возвращает шаблон компонента
     toHTML() {
         return ''
+    }
+
+    $emit(event, ...args) {
+        this.emitter.emit(event, ...args)
+    }
+
+    $on(event, fn) {
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubscribes.push(unsub)
     }
 
     init() {
@@ -17,5 +34,6 @@ export class ExcelComponent extends DomListener{
 
     destroy() {
         this.removeDOMListeners()
+        this.unsubscribes.forEach(unsub => unsub())
     }
 }
